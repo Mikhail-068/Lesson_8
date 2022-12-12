c8838e39aa4b1961f67df1db53f1566029a598fe
Mihail_AI <m89531276499@gmail.com>
2022-12-12 03:10:03 +0300

    delete

--- blocks.py
+++
@@ -1,8 +0,0 @@
-if 2 > 1:
-    # a = 10
-    pass
-else:
-    a = 20
-
-print(a)
-print(a + 30)
--- filemanager.py
+++
@@ -1,9 +0,0 @@
-from decorators import  add_separators
-
-
-@add_separators
-def manager_f():
-    print('Создатель программы')
-
-
-manager_f()
